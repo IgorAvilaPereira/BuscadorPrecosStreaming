4bfa5de5ab8da0ded86a19152065f31626e053b9
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2023-11-29 09:09:36 -0300

    ok

--- app.py
+++ app.py
@@ -4,56 +4,7 @@ import flet as ft
 def main(page):
     page.title = "BuscadorPrecosStreaming - Desenvolvido por Igor Avila Pereira"
     page.padding = 50
-    
-    tx = ft.Text("Atualizando Preços dos Streamings. Espere um pouco...")
-    
-    images_loading = ft.GridView(
-        expand=1,
-        runs_count=5,
-        max_extent=150,
-        child_aspect_ratio=1.0,
-        spacing=5,
-        run_spacing=5
-    )
-    images_loading.controls.append(ft.Image(src=f"assets/icons/loading.gif", width=100, height=100, fit=ft.ImageFit.CONTAIN))    
-    page.add(tx)
-    page.add(images_loading)
-    page.update()
 
-    buscador = BuscadorPrecosStreaming(False)    
-    # method_list = [method for method in dir(BuscadorPrecosStreaming) if method.startswith('__') is False]        
-    # method_list = ["paramountPlus"]    
-    method_list = ["primeVideo", "appleTvPlus", "playplus", "hboMax", "netFlix", "paramountPlus", "globoPlay", "discoveryPlus", "disneyPlus"]    
-    streamings = []
-    for method in method_list:                
-        price = float(eval("buscador."+method+"()"))                
-        if (price != -1):
-            tx.value = "Atualizando mensalidade dos Streamings: "+method+"..."
-            streamings.append(dict(name = method, price = price))
-            page.update()    
-
-    if (len(streamings)>0):
-        streamings = sorted(streamings, key=lambda k: k['price'])                  
-        tx.visible = False
-        images_loading.visible = False
-        images = ft.GridView(
-            expand=1,
-            runs_count=5,
-            max_extent=150,
-            child_aspect_ratio=1.0,
-            spacing=5,
-            run_spacing=5,
-        )                
-        t = ft.Text()
-        page.add(
-            ft.Text("Qual é seu orçamento mensal para Streamings?"),
-            ft.Slider(min=0, max=500, divisions=100, label="R$ {value}", on_change=slider_changed), t)    
-        page.add(images)
-        page.update()
-    else:
-        tx.value = "Problema de Atualização: Nenhuma mensalidade foi obtida.\nVerifique sua conexão com a internet"
-        page.update()    
-  
     def slider_changed(e):
         t.value = f"Orçamento Mensal para Entretenimento: R$ {int(e.control.value)}"   
         images.controls = []        
@@ -84,6 +35,56 @@ def main(page):
                 images.controls.append(ft.Text("R$ "+str(price)))
             i = i + 1  
         page.update()
+
+    tx = ft.Text("Atualizando Preços dos Streamings. Espere um pouco...")    
+    images_loading = ft.GridView(
+        expand=1,
+        runs_count=5,
+        max_extent=150,
+        child_aspect_ratio=1.0,
+        spacing=5,
+        run_spacing=5
+    )
+    images_loading.controls.append(ft.Image(src=f"assets/icons/loading.gif", width=100, height=100, fit=ft.ImageFit.CONTAIN))    
+    page.add(tx)
+    page.add(images_loading)
+    page.update()
+
+    buscador = BuscadorPrecosStreaming(False)    
+    # method_list = [method for method in dir(BuscadorPrecosStreaming) if method.startswith('__') is False]        
+    # method_list = ["paramountPlus"]    
+    method_list = ["primeVideo", "appleTvPlus", "playplus", "hboMax", "netFlix", "paramountPlus", "globoPlay", "discoveryPlus", "disneyPlus"]    
+    streamings = []
+    for method in method_list:                
+        price = float(eval("buscador."+method+"()"))                
+        if (price != -1):
+            tx.value = "Atualizando mensalidades dos Streamings: "+method+"..."
+            streamings.append(dict(name = method, price = price))
+            page.update()    
+
+    if (len(streamings)>0):
+        streamings = sorted(streamings, key=lambda k: k['price'])                  
+        tx.visible = False
+        images_loading.visible = False
+        images = ft.GridView(
+            expand=1,
+            runs_count=5,
+            max_extent=150,
+            child_aspect_ratio=1.0,
+            spacing=5,
+            run_spacing=5,
+        )                
+        t = ft.Text()
+        page.add(
+            ft.Text("Qual é seu orçamento mensal para Streamings?"),
+            ft.Slider(min=0, max=500, divisions=100, label="R$ {value}", on_change=slider_changed), t)    
+        page.add(images)
+        page.update()
+    else:
+        tx.value = "Problema de Atualização: Nenhuma mensalidade foi obtida.\nVerifique sua conexão com a internet"
+        page.update()    
+  
+ 
 # desktop
 # pipreqs ./
 ft.app(target=main, assets_dir="assets")
